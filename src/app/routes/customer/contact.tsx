8b7dca04bf1b3f1feefe67372d33c1897959ded6
denbahrul <ulum0326@gmail.com>
2024-12-03 22:37:12 +0700

    fix: package UI and contact UI

--- src/app/routes/customer/contact.tsx
+++ src/app/routes/customer/contact.tsx
@@ -1,16 +1,16 @@
-import { TbMapSearch } from "react-icons/tb";
+import { PiMapPinArea } from "react-icons/pi";
 import { IoMailOutline, IoCallOutline, IoLogoWhatsapp } from "react-icons/io5";
 
 export default function Contact() {
   return (
     <div className="m-auto max-w-screen-xl p-6 pt-24">
       {/* Title */}
-      <p className="text-3xl font-extrabold text-gray-800 md:text-4xl">
+      <p className="text-center text-3xl font-extrabold text-gray-800">
         Contact
       </p>
       <div className="mt-8 grid grid-cols-1 gap-10 md:grid-cols-2">
         {/* Contact Details */}
-        <div className="space-y-6 rounded-lg bg-white p-6 shadow-md md:p-8">
+        <div className="space-y-6 rounded-lg p-6">
           <div>
             <p className="text-2xl font-bold text-gray-800 md:text-3xl">
               Bambang Agus H
@@ -18,39 +18,53 @@ export default function Contact() {
           </div>
           <div className="space-y-4">
             <div className="flex items-center gap-4">
-              <IoCallOutline size={28} className="text-blue-500" />
-              <p className="text-lg text-gray-700">
-                <span className="font-medium">Telpon:</span> 085743311822
-              </p>
+              <div>
+                <IoCallOutline size={28} className="w-8 text-black" />
+              </div>
+              <div>
+                <p className="text-sm font-semibold text-gray-700">Telpon :</p>
+                <p className="text-lg font-semibold">085743311822</p>
+              </div>
             </div>
             <div className="flex items-center gap-4">
-              <IoLogoWhatsapp size={28} className="text-green-500" />
-              <p className="text-lg text-gray-700">
-                <span className="font-medium">WhatsApp:</span> 085743311822
-              </p>
+              <div>
+                <IoLogoWhatsapp size={28} className="w-8 text-black" />
+              </div>
+              <div>
+                <p className="text-sm font-semibold text-gray-700">
+                  WhatsApp :
+                </p>
+                <p className="text-lg font-semibold">085743311822</p>
+              </div>
             </div>
             <div className="flex items-center gap-4">
-              <IoMailOutline size={28} className="text-indigo-500" />
-              <p className="text-lg text-gray-700">
-                <span className="font-medium">Email:</span>{" "}
-                persewaandewa@gmail.com
-              </p>
+              <div>
+                <IoMailOutline size={28} className="w-8 text-black" />
+              </div>
+              <div>
+                <p className="text-sm font-semibold text-gray-700">Email :</p>
+                <p className="text-lg font-semibold">persewaandewa@gmail.com</p>
+              </div>
             </div>
             <div className="flex items-start gap-4">
-              <TbMapSearch size={28} className="text-red-500" />
-              <p className="text-lg text-gray-700">
-                <span className="font-medium">Alamat:</span> Karanggeng
-                RT04/RW32, Purwobinangun Pakem, Sleman, Yogyakarta
-              </p>
+              <div>
+                <PiMapPinArea size={28} className="w-8 text-black" />
+              </div>
+              <div>
+                <p className="text-sm font-semibold text-gray-700">Alamat :</p>
+                <p className="text-lg font-semibold">
+                  Karanggeng RT04/RW32, Purwobinangun Pakem, Sleman, Yogyakarta
+                </p>
+              </div>
             </div>
           </div>
           {/* Divider */}
           <hr className="border-gray-300" />
           {/* Action Button */}
-          <div className="flex justify-center">
+          <div className="flex">
             <a
               href="tel:085743311822"
-              className="rounded-lg bg-indigo-600 px-6 py-3 text-sm font-semibold text-white shadow-md hover:bg-indigo-500"
+              className="rounded-lg bg-black px-6 py-3 text-sm font-semibold text-white shadow-md hover:bg-indigo-500"
             >
               Hubungi Sekarang
             </a>
@@ -60,10 +74,15 @@ export default function Contact() {
         {/* Map */}
         <div className="overflow-hidden rounded-lg shadow-md">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.064625276419!2d109.90771869678953!3d-7.233469599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e700da120c1333d%3A0xbfaa9d1832340de9!2sVila%20tugu%20langit!5e0!3m2!1sid!2sid!4v1668151351280!5m2!1sid!2sid"
-            className="h-[400px] w-full"
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3954.1515057363345!2d110.3809250759307!3d-7.666855775879649!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwNDAnMDAuNyJTIDExMMKwMjMnMDAuNiJF!5e0!3m2!1sid!2sid!4v1733238733289!5m2!1sid!2sid"
+            className="h-full w-full"
             loading="lazy"
           ></iframe>
+          {/* <iframe
+            src="https://maps.app.goo.gl/aNZoSRiLsDMup99z6"
+            className="h-[400px] w-full"
+            loading="lazy"
+          ></iframe> */}
         </div>
       </div>
     </div>
--- src/components/ui/package-card.tsx
+++ src/components/ui/package-card.tsx
@@ -6,7 +6,7 @@ export default function PackageCard({
   name: string;
 }) {
   return (
-    <div className="bg-green grid grid-cols-2 gap-1 rounded-lg p-4">
+    <div className="grid grid-cols-2 gap-2 rounded-lg bg-[#e2fde2] p-4">
       <div className="flex flex-col justify-between">
         <div>
           <p className="text-lg font-bold">{name}</p>
@@ -17,6 +17,9 @@ export default function PackageCard({
         </div>
 
         <p className="text-md font-bold">Rp.3.000.000</p>
+        <a className="rounded-md bg-black py-1 text-center text-sm text-white">
+          Detail
+        </a>
       </div>
       <img
         className="h-52 w-full rounded-lg object-cover"
